175331d6bcd1786a310979159e92eeb8860f4d2a
qianfeng <qianfeng@Macintosh-2.local>
2014-10-15 14:22:03 +0800

    Initial Commit

---
+++ yy/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  yy
+//
+//  Created by qianfeng on 14-10-15.
+//  Copyright (c) 2014年 千峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yy
+//
+//  Created by qianfeng on 14-10-15.
+//  Copyright (c) 2014年 千峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
